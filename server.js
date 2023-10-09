697de9f50532a618091dd42a67dbf24a88e10e66
Noah Nemhed <noahnemhed@hotmail.com>
2023-10-10 00:09:15 +0200

    Add body-parser module

--- server.js
+++ server.js
@@ -19,8 +19,8 @@ app.use(bodyParser.json())
 app.use(cors())
 
 
-app.listen(3000, () => {
-    console.log("Server is running on port 3000")
+app.listen(process.env.PORT || 3000, () => {
+    console.log(`Server is running on port ${process.env.PORT}`)
 })
 
 app.post("/signin", (req,res) => {
